31bb7451b14347ccf7e1088de9f0caf303fd2109
nidjo pavlovic <e_nidjo@hotmail.com>
2020-09-07 10:09:12 +0200

    hej

--- Guestbook/Program.cs
+++ Guestbook/Program.cs
@@ -10,6 +10,7 @@ namespace Guestbook
             Console.WriteLine(DateTime.Now.ToString("dd-MM-yyyy") + ": Martina was here!");
             Console.WriteLine(DateTime.Now.ToString("dd-MM-yyyy") + ": Martina was here again!");
             Console.WriteLine(DateTime.Now.ToString("dd-MM-yyyy") + ": David Crosson was here!");
+            Console.WriteLine(DateTime.Now.ToString("dd-MM-yyyy") + ": Nikola Pavlovic was here!");
 
 
         }
