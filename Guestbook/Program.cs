424766be5dfddbb6890298a3d2072f60ef480f0e
Lucas <lucas.ersson@student.kyh.se>
2020-09-07 10:52:48 +0200

    kommentar

--- Guestbook/Program.cs
+++ Guestbook/Program.cs
@@ -14,6 +14,7 @@ namespace Guestbook
             Console.WriteLine(DateTime.Now.ToString("dd-MM-yyyy") + ": Pedro Lagerström was here!");
             Console.WriteLine(DateTime.Now.ToString("dd-MM-yyyy") + ": Dylan Abdul-Rahman was here!");
             Console.WriteLine(DateTime.Now.ToString("dd-MM-yyyy") + ": Meda Krasniqi was here!");
+            Console.WriteLine(DateTime.Now.ToString("dd-MM-yyyy") + ": Lucas Ersson was here!");
 
             Console.WriteLine(DateTime.Now.ToString("dd-MM-yyyy") + ": Martina was here again!");
 
